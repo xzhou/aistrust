0dbbd5d11255b388ff6a80361bd51c1bde259d3a
zhou.xiaoyong <zhou.xiaoyong@43d9226e-1bc5-11de-be07-9135ae5da72a>
2009-04-06 04:16:46 +0000

    add ant file to compile

---
+++ code/src/ais/MainTest.java
@@ -0,0 +1,13 @@
+package ais;
+
+public class MainTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("hello, fdf");
+	}
+
+}
